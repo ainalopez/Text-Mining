409a5389f9b29e5abe7675f42f1827140827f9d5
halliwelln <nicholas.halliwell@barcelonagse.eu>
2016-04-19 18:59:03 +0200

    commit
    get links as list

--- web-scraper.py
+++ web-scraper.py
@@ -20,12 +20,21 @@ import sys
 import time, random
 
 url = 'http://www.yelp.com/search?find_desc=Restaurants&find_loc=barcelona'
+
+#read in html as string
 open_url = urllib2.urlopen(url).read()
 
+#parse html
 soup = bs(open_url, "html.parser")
 
 #get all restaurant reviews
 all_href = [links.get('href') for links in soup.find_all('a')]
+all_href_str = [str(links) for links in all_href]
 
-all_links = [link for link in all_href if "/biz" in link]
 
+all_links = [link for link in all_href_str if "/biz/" in link and "?" not in link and "%" not in link]
+
+links = list(set(all_links))
+
+for i in links:
+    print i
