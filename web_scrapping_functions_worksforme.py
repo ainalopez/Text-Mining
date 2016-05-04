722ee9f2a0b36ae50a46c4002ce84350f3750366
Aina Lopez <aina.lopez@barcelonagse.eu>
2016-05-04 16:25:22 +0200

    scrapping version that works for me

--- web_scrapping_functions_worksforme.py
+++ web_scrapping_functions_worksforme.py
@@ -9,22 +9,24 @@ import os
 
 
 '''
-    Input: URL of restaurant
-    Output: JSON file with
-    To scrape a webpage, the function getData scrapes the site, and the function createJSON writes the data in a file.
-    Elements in JSON:
-    - Review
-    - Rating
-    - User name
-    - User Location
-    - Number of friends of user
-    - Number of reviews the user wrote
-    - Restaurant name
-    - Ranking of the review in restaurant
-    - Type of restaurant (list)
-    - Price category (Number of €€€€ in restaurant page)
-    
-    '''
+
+ Input: URL of restaurant
+ Output: JSON file with 
+
+ To scrape a webpage, the function getData scrapes the site, and the function createJSON writes the data in a file. 
+ Elements in JSON:
+ - Review
+ - Rating
+ - User name
+ - User Location
+ - Number of friends of user
+ - Number of reviews the user wrote
+ - Restaurant name
+ - Ranking of the review in restaurant
+ - Type of restaurant (list)
+ - Price category (Number of €€€€ in restaurant page)
+ 
+'''
 
 
 
@@ -80,9 +82,9 @@ def getPrice(soup):
 def getData(url):
     
     # Get html code
-    html = urllib.urlopen(url).read()
+    url = 'http://' + url
+    html = urlopen(url).read()
     soup = BeautifulSoup(html)
-    
     # Get all the pages with reviews
     pages=soup.select(".page-of-pages")[0].text
     num=str(pages)
@@ -91,17 +93,11 @@ def getData(url):
     index=range(20,num*20,20)
     urls=[url + "?start=" + str(sufix) for sufix in index]
     urls.insert(0,url)
-    
     # Characteristics of the restaurant
-    
     # Type of restaurant
     types=getType(soup)
-    
     # Price of restaurant (number of €)
     price=getPrice(soup)
-    
-    
-    
     # Initialize list
     ALL_REVIEWS=[]
     ALL_STARS=[]
@@ -109,82 +105,75 @@ def getData(url):
     ALL_USER_LOCATION=[]
     ALL_NUM_FRIENDS=[]
     ALL_NUM_REVIEWS=[]
-    
-    
     for link in urls:
         # Get soup
         html = urllib.urlopen(link).read()
         soup = BeautifulSoup(html)
-        
         # Review
         new_reviews=getReview(soup)
         ALL_REVIEWS=ALL_REVIEWS + new_reviews
-        
         # Stars
         new_stars=getStars(soup)
         ALL_STARS=ALL_STARS + new_stars
-        
         # User Names
         new_names=getUserName(soup)
         ALL_USER_NAME=ALL_USER_NAME + new_names
-        
-        # User Location
+         # User Location
         new_location=getUserLocation(soup)
         ALL_USER_LOCATION=ALL_USER_LOCATION + new_location
-        
         # Number of Friends
         new_friends=getNumFriends(soup)
         ALL_NUM_FRIENDS=ALL_NUM_FRIENDS + new_friends
-        
         # Number of Reviews
         new_num_reviews=getNumReviews(soup)
         ALL_NUM_REVIEWS=ALL_NUM_REVIEWS + new_num_reviews
-        
         # Sleeping time
         delay = random.randint(1,2)
         time.sleep(delay)
-    
-    
     # Combine all lists
     data = zip(ALL_REVIEWS, ALL_STARS, ALL_USER_NAME, ALL_USER_LOCATION, ALL_NUM_FRIENDS, ALL_NUM_REVIEWS)
-    
     return data, types, price
-
-
+    
+    
 #Create JSON: the name of the file is the name of the restaurant
 
-def createJSON(data,url):
-    rest_name=url.split("/")[-1]
-        for i in range(len(data)):
-            file_name=rest_name+"_"+str(i)+".json"
-            out_file = open(file_name,"w")
-            doc = list(data[i])
-            doc.append(rest_name)
-            doc.append(i+1)
-            doc.append(tuple(types))
-            doc.append(int(str(price[0])))
-            json_str = json.dumps(doc, indent = 4)
-            out_file.write(json_str)
-            out_file.close()
+def createJSON(data,url):  
+ rest_name=url.split("/")[-1]
+ for i in range(len(data)):
+     file_name=rest_name+"_"+str(i)+".json"
+     out_file = open(file_name,"w")
+     doc = list(data[i])
+     doc.append(rest_name)
+     doc.append(i+1)
+     doc.append(tuple(types))
+     if len(price)>0: 
+     	doc.append(int(str(price[0])))
+     json_str = json.dumps(doc, indent = 4)
+     out_file.write(json_str)
+     out_file.close()
 
 
-with open("accepted_yelp_links", "r") as fi:
+
+
+with open("/Users/ainalopez/Desktop/Text-Mining/accepted_yelp_links", "r") as fi:
     links = json.load(fi)
 
 #0-300 Nick
 #300-500 Yaro
-#500-len(links)+1 Aina
-for url in links[0:300]:
-    
+#500-len(links)-1 Aina
+for url in links[1:(len(links)-1)]:
+    delay = random.randint(1,2)
+    time.sleep(delay)
     try:
-        os.chdir("/Users/Nick/Desktop/Links")
+        os.chdir("/Users/ainalopez/Desktop/Links")
         data, types, price=getData(url)
-        createJson(data, url)
-    
+        createJSON(data, url)
+        
     except:
-        os.chdir("/Users/Nick/Desktop")
-        with open("rejected.txt", "r") as f:
+        os.chdir("/Users/ainalopez/Desktop/Rejected")
+        with open("rejected.txt", "a") as f:
             f.write(url)
+            f.write("\n")
 
 # Read file
 #with open('quimet-and-quimet-barcelona_0.json') as data_file:    
